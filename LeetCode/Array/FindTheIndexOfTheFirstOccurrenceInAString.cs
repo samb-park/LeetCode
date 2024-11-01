e7f5694de5685ef4fd32af1b985ae5107b23bc3d
samb-park <sangbong.park@outlook.com>
2024-11-01 09:44:40 -0400

    6. Zigzag Conversion 28. Find the Index of the First Occurrence in a String

---
+++ LeetCode/Array/FindTheIndexOfTheFirstOccurrenceInAString.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class FindTheIndexOfTheFirstOccurrenceInAString : ILeetCodeProblem
+    {
+        public void RunTests()
+        {
+            Console.WriteLine(StrStr("a", "a"));
+        }
+
+        public int StrStr(string haystack, string needle)
+        {
+
+            for (int i = 0; i <= haystack.Length - needle.Length; i++)
+            {
+                if (haystack[i] == needle[0])
+                {
+                    int j = 1;
+                    for (j = 1; j < needle.Length; j++)
+                    {
+                        if (haystack[i + j] != needle[j])
+                            break;
+                    }
+
+                    if (needle.Length == j)
+                        return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ LeetCode/Array/ZigzagConversion.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCode.Array
+{
+    internal class ZigzagConversion : ILeetCodeProblem
+    {
+        public void RunTests()
+        {
+            var Converted = Convert("AB", 1);
+            Console.WriteLine(Converted);
+        }
+        public string Convert(string s, int numRows)
+        {
+            List<StringBuilder> outputs = new List<StringBuilder>();
+            int currentIndex = 0;
+            bool isGoingDown = true;
+
+
+            for (int i = 0; i < numRows; i++)
+            {
+                outputs.Add(new StringBuilder());
+            }
+
+            foreach (var c in s)
+            {
+                if(numRows == 1)
+                    currentIndex = 0;
+
+                outputs[currentIndex].Append(c);
+
+                if (isGoingDown && currentIndex == numRows - 1)
+                {
+                    isGoingDown = false;
+                }
+                else if (!isGoingDown && currentIndex == 0)
+                {
+                    isGoingDown = true;
+                }
+
+                if (isGoingDown)
+                {
+                    currentIndex++;
+                }
+                else
+                {
+                    currentIndex--;
+                }
+            }
+
+            var result = "";
+
+            foreach (var output in outputs)
+            {
+                result += output.ToString();
+            }
+            return result;
+        }
+    }
+}
